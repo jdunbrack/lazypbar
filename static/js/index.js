a6afff5ecc28280301d3748c899f59f686b38e12
Jordan Dunbrack <jordan.dunbrack@gmail.com>
2021-09-02 16:33:06 -0700

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,36 @@
+$('document').ready(() => {
+    $('#about-content').show();
+    $('#residents-content').hide();
+    $('#donate-content').hide();
+})
+
+$('.info-button#about').click(() => {
+    $('#about-content').show();
+    $('#residents-content').hide();
+    $('#donate-content').hide();
+    
+    $('.info-button#about').addClass('active');
+    $('.info-button#residents').removeClass('active');
+    $('.info-button#donate').removeClass('active');
+    
+})
+
+$('.info-button#residents').click(() => {
+    $('#about-content').hide('slow');
+    $('#residents-content').show('slow');
+    $('#donate-content').hide();
+    
+    $('.info-button#about').removeClass('active');
+    $('.info-button#residents').addClass('active');
+    $('.info-button#donate').removeClass('active');
+})
+
+$('.info-button#donate').click(() => {
+    $('#about-content').hide();
+    $('#residents-content').hide();
+    $('#donate-content').show();
+    
+    $('.info-button#about').removeClass('active');
+    $('.info-button#residents').removeClass('active');
+    $('.info-button#donate').addClass('active');
+})
